7b8ed785f0fdfc845f6d54bf9302b63f7143fd1b
duncandevs <duncandevs@gmail.com>
2017-01-18 23:36:24 -0500

    create dbs, change my recipes page

--- recipe-nav/app/controllers/users_controller.rb
+++ recipe-nav/app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   end
   def show
     @user = User.find(params[:id])
+    @recipes = @user.recipes
   end
   def create
     @user = User.new(user_params)
