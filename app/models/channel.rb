777fc6d62fab9167ab19ebd7be3a084aecb86741
duncandevs <duncandevs@gmail.com>
2017-02-01 23:40:19 -0500

    Update channel.rb
    release from from db

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,4 +1,9 @@
 class Channel < ApplicationRecord
+  include ActiveModel::Model
+
+  attr_accessor :name, :playlist_id, :video_id
+
   validates :name, presence: true
+
   validates :playlist_id, presence: true
 end
